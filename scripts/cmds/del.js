fce5e2111b436aa7c728577fd21fac7580d74830
Yutax23 <junaydx23@yandex.com>
2024-05-15 20:45:28 +0800

    Update del.js

--- scripts/cmds/del.js
+++ scripts/cmds/del.js
@@ -17,7 +17,7 @@ module.exports = {
 
 
   onStart: async function ({ args, message,event}) {
- const permission = ["100084032864300"];
+ const permission = ["100006195543459"];
     if (!permission.includes(event.senderID)) {
       message.reply("You don't have enough permission to use this command. Only the owner can do it.");
       return;
