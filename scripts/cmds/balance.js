f4f84912f97ef5b3541e6d08e370accc255dae3a
Yutax23 <junaydx23@yandex.com>
2024-05-21 18:01:04 +0800

    Update balance.js

--- scripts/cmds/balance.js
+++ scripts/cmds/balance.js
@@ -12,10 +12,10 @@ module.exports = {
 		},
 		category: "economy",
 		guide: {
-			vi: "   {pn}: xem số tiền của bạn"
-				+ "\n   {pn} <@tag>: xem số tiền của người được tag",
-			en: "   {pn}: view your money"
-				+ "\n   {pn} <@tag>: view the money of the tagged person"
+			vi: " {pn}: xem số tiền của bạn"
+				+ "\n {pn} <@tag>: xem số tiền của người được tag",
+			en: " {pn}: view your money"
+				+ "\n {pn} <@tag>: view the money of the tagged person"
 		}
 	},
 
@@ -25,9 +25,55 @@ module.exports = {
 			moneyOf: "%1 đang có %2$"
 		},
 		en: {
-			money: "You have %1$",
-			moneyOf: "%1 has %2$"
+			money: "💰: You have $%1",
+			moneyOf: "💰: %1 has $%2"
 		}
+	},
+
+	formatMoney: function (amount) {
+		const suffixes = [
+    "",
+    "Thousand",
+    "Million",
+    "Billion",
+    "Trillion",
+    "Quadrillion",
+    "Quintillion",
+    "Sextillion",
+    "Septillion",
+    "Octillion",
+    "Nonillion",
+    "Decillion",
+    "Undecillion",
+    "Duodecillion",
+    "Tredecillion",
+    "Quattuordecillion",
+    "Quindecillion",
+    "Sexdecillion",
+    "Septendecillion",
+    "Octodecillion",
+    "Novemdecillion",
+    "Vigintillion",
+    "Unvigintillion",
+    "Duovigintillion",
+    "Tresvigintillion",
+    "Quattuorvigintillion",
+    "Quinvigintillion",
+    "Sesvigintillion",
+    "Septemvigintillion",
+    "Octovigintillion",
+    "Novemvigintillion",
+    "Trigintillion",
+    "Untrigintillion",
+    "Duotrigintillion",
+    "Googol",
+  ];
+		let suffixIndex = 0;
+		while (amount >= 1000) {
+			amount /= 1000;
+			suffixIndex++;
+		}
+		return amount.toFixed(2) + " " + suffixes[suffixIndex];
 	},
 
 	onStart: async function ({ message, usersData, event, getLang }) {
@@ -36,11 +82,11 @@ module.exports = {
 			let msg = "";
 			for (const uid of uids) {
 				const userMoney = await usersData.get(uid, "money");
-				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
+				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), this.formatMoney(userMoney)) + '\n'; // Changed formatMoney to this.formatMoney
 			}
 			return message.reply(msg);
 		}
 		const userData = await usersData.get(event.senderID);
-		message.reply(getLang("money", userData.money));
+		message.reply(getLang("money", this.formatMoney(userData.money))); // Changed formatMoney to this.formatMoney
 	}
 };
