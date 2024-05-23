51856544debb6641b42b3ba8e1ca7e99556f83db
Yutax23 <junaydx23@yandex.com>
2024-05-23 16:05:18 +0800

    Update Create.js

--- scripts/cmds/Create.js
+++ scripts/cmds/Create.js
@@ -12,7 +12,7 @@ module.exports = {
     role: 0,
     shortDescription: "calicomix",
     longDescription: "calicomix",
-    category: "image",
+    category: "🖼️| IMAGE",
     guide: {
       en: "{p}meina [prompt]"
     }
