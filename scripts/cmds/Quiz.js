0b54b08759a4c6e38f0cf16974a0406fd9af24ef
Yutax23 <junaydx23@yandex.com>
2024-05-24 05:40:08 +0800

    Update Quiz.js

--- scripts/cmds/Quiz.js
+++ scripts/cmds/Quiz.js
@@ -16,7 +16,7 @@ module.exports = {
       vi: "",
       en: ""
     },
-    category: "games",
+    category: "🎮| GAMES",
     guide: {
       en: "{pn} <category>\n{pn} rank\n-view your rank\n{pn} leaderboard\n-view top players\nto submit a bug report or feedback\n\n just type:\nquizr <your message>\n"
     },
