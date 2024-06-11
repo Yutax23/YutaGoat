4d71b386b2cf2f9131171213c90deaea6786b0e3
Yutax23 <junaydx23@yandex.com>
2024-06-11 11:20:15 +0800

    Update antichangeinfobox.js

--- scripts/cmds/antichangeinfobox.js
+++ scripts/cmds/antichangeinfobox.js
@@ -2,7 +2,7 @@ const { getStreamFromURL, uploadImgbb } = global.utils;
 
 module.exports = {
 	config: {
-		name: "antichangeinfobox",
+		name: "acibix",
 		version: "1.9",
 		author: "NTKhang",
 		countDown: 5,
