a142adeebcab897e3ce0f3c0ace7327174bc3222
Yutax23 <junaydx23@yandex.com>
2024-06-11 11:32:37 +0800

    Update sv.js

--- scripts/cmds/sv.js
+++ scripts/cmds/sv.js
@@ -1,36 +1,35 @@
-async function sv({ message: m, args: a, event: e, }) {
+async function sv({ message: m, args: a, event: e }) {
   const k = a.join(" ");
   if (!k) {
     m.reaction("❌", e.messageID, () => {}, true);
-    m.reply(this.config.guide);
+    m.reply(module.exports.config.guide);
     return;
   }
   m.reaction("⏱", e.messageID, () => {}, true);
   const b = require("yt-search");
   let [d, ...f] = k.split(" ");
   f = f.join(" ");
-  if (!f)
-  return m.reply(this.config.guide);
+  if (!f) return m.reply(module.exports.config.guide);
   const g = await b(f);
   const h = g.videos[0];
   const i = h.url;
   try {
-    switch (d) {
-    case "-s":
-    case "sing": {
-      m.reply({ attachment: await global.utils.getStreamFromURL(`https://deku-rest-api-3ijr.onrender.com/ytdl?url=${i}&type=mp3`, 'cache')});
-    m.reaction("✅", e.messageID, () => {}, true);
-return;
-
-    }
-    case "-v":
-    case "video": {
-      m.reply({ attachment: await global.utils.getStreamFromURL(`https://deku-rest-api-3ijr.onrender.com/ytdl?url=${i}&type=mp4`, 'cache')});
-    m.reaction("✅", e.messageID, () => {}, true);
-return;
-    }
-    default: {
-      if (!d) { return m.reply(this.config.guide); }
+    switch (d.toLowerCase()) {
+      case "-s":
+      case "sing": {
+        m.reply({ attachment: await global.utils.getStreamFromURL(`https://deku-rest-api-3ijr.onrender.com/ytdl?url=${i}&type=mp3`, 'cache') });
+        m.reaction("✅", e.messageID, () => {}, true);
+        return;
+      }
+      case "-v":
+      case "video": {
+        m.reply({ attachment: await global.utils.getStreamFromURL(`https://deku-rest-api-3ijr.onrender.com/ytdl?url=${i}&type=mp4`, 'cache') });
+        m.reaction("✅", e.messageID, () => {}, true);
+        return;
+      }
+      default: {
+        if (!d) return m.reply(module.exports.config.guide);
+      }
     }
   } catch (error) {
     m.reaction("❌", e.messageID, () => {}, true);
@@ -51,7 +50,6 @@ module.exports = {
   },
   onStart: sv
 };
-
 const { GoatWrapper } = require('fca-liane-utils');
 const wrapper = new GoatWrapper(module.exports);
 
