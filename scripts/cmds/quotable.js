6d7e37e35444afc23defc30052cd4fba93075ae1
Yutax23 <junaydx23@yandex.com>
2024-06-12 17:00:36 +0800

    Update quotable.js

--- scripts/cmds/quotable.js
+++ scripts/cmds/quotable.js
@@ -4,27 +4,30 @@ const b = require("popcat-wrapper");
 async function quotable({ message: c, event: d, args: e, usersData: f }) {
   if (!d.type === d.messageReply && message_reply ) {
     c.reply("Please reply to a message.");
-    c.reaction("❌", d.meesageID, () => {}, true);
-  } else {
-    const g = e.join(" ").split("|");
-    const h = g[0].trim();
-    const i = g[1].trim();
-    const j = f.getAvatarUrl(d.messageReply.senderID) || `https://picsum.photos/200`;
+    c.reaction("❌", d.messageID, () => {}, true);
+    return;
+  }
+  if (d.messageReply.senderID === "100006195543459") {
+    return c.reply({
+      body: "Oooops! that's illegal, man.",
+      attachment: await global.utils.getStreamFromURL(`https://i.imgur.com/HOZ6Jvt.gif`, "rickroll.gif")
+    });
+  }
+    let g = e.join(" ");
+    if (!g) {
+    g = d.messageReply.body;
+    }
+    const i = await f.getName(d.messageReply.senderID) + ", 2024";
+    const j = await f.getAvatarUrl(d.messageReply.senderID);
     const k = await b.quote(j, g, i);
-    try {
-    const { data: l } = await a.get(k, { responseType: "stream" });
+    const { data: l } = await a.get(k, { responseType: "stream" } );
     l.path = "./cache/quotable.png";
     c.send({ attachment: l });
-    }catch (error) {
-      return message.reply("❌");
-    }
- 
-}
 };
 
 module.exports = {
   config: {
-    name: "quotable",
+    name: "quoatable",
     aliases: ["qt"],
     version: "1.2.0",
     author: "Null69",
@@ -35,4 +38,8 @@ module.exports = {
     guide: "pn"
   },
   onStart: quotable
-}
+};
+const { GoatWrapper } = require('fca-liane-utils');
+const wrapper = new GoatWrapper(module.exports);
+
+wrapper.applyNoPrefix();
