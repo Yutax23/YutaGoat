7911d044e5b9ac31591a3114de9b3b54c125d439
Yutax23 <junaydx23@yandex.com>
2024-06-13 16:07:56 +0800

    Create yt.js

---
+++ scripts/cmds/yt.js
@@ -0,0 +1,17 @@
+async function yt({ message, args }) {
+  try {
+  message.reply({ attachment: await global.utils.getStreamFromURL(`https://jipitiforow-efec7e4faff4.herokuapp.com/ytdl/sing?title=${encodeURIComponent(args.join(" "))}`, `${args.join(" ")}.mp4`)});
+  } catch (error) {
+    return message.reply(error);
+  }
+};
+module.exports = {
+  config: {
+    name: "yt",
+    author: "null69",
+    description: "play music",
+    category: "media",
+    guide: "{pn} title"
+  },
+  onStart: yt
+                                                                                                                                                                             
