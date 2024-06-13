6df6f78907cd044cdda88861a4a2bac478087d26
Yutax23 <junaydx23@yandex.com>
2024-06-13 16:06:02 +0800

    Create music.js

---
+++ scripts/cmds/music.js
@@ -0,0 +1,16 @@
+async function music({ message, args }) {
+  try {
+  message.reply({ attachment: await global.utils.getStreamFromURL(`https://jipitiforow-efec7e4faff4.herokuapp.com/ytdl/sing?title=${encodeURIComponent(args.join(" "))}`, `${args.join(" ")}.mp3`)});
+  } catch (error) {
+    return message.reply(error);
+  }
+};
+module.exports = {
+  config: {
+    name: "music",
+    author: "null69",
+    description: "play music",
+    category: "media",
+    guide: "{pn} title"
+  },
+  onStart: music
