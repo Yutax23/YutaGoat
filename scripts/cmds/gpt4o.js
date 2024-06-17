87c266afbdaffdc15e92a8fa65ccf15911dcf466
Yutax23 <junaydx23@yandex.com>
2024-06-18 05:58:16 +0800

    Update gpt4o.js

--- scripts/cmds/gpt4o.js
+++ scripts/cmds/gpt4o.js
@@ -1,44 +1,66 @@
-async function supot({ message, args, event, commandName }) {
-  message.reaction("⏳", event.messageID, () => {}, true);
-  const text = args.join(" ");
-  if (!text) {
-    message.reaction("❌", event.messageID, () => {}, true);
-    message.reply(this.config.guide);
-    return;
+async function gpt4o({ event, message, args, commandName, api, getLang }) {
+  const prompt = args.join(" ");
+  if (!prompt) {
+    return message.reply(getLang("usage"));
   }
-  try {
-    const response = await getResponse(text);
-    message.reaction("✅", event.messageID, () => {}, true);
-    return message.reply(response);
-  }catch (error) {
-    return;
-  }
-  
-};
+  const uid = event.senderID;
+  const axios = require("axios");
+try {
+    const res = await axios.get(`https://for-devs.onrender.com/api/gpt4o`, {
+      params: {
+        query: encodeURIComponent(prompt),
+        uid: uid,
+        apikey: "api1"
+      }
+    });
 
-async function getResponse(text) {
-  try {
-    const axios = require("axios");
-    const res = await axios.get(`https://jipitiforow-efec7e4faff4.herokuapp.com/api/chat?question=${text}`);
-    return res.data;
-  }catch (error) {
-    return message.reply(`❌ | ${error}`);
+    if (res.data.result) {
+      message.reaction("✅", event.messageID);
+      const text = res.data.result;
+       message.reply(getLang("answer", text), (err, info) => {
+        if (!err) {
+    global.GoatBot.onReply.set(info.messageID, {
+      commandName,
+      author: uid,
+      messageID: info.messageID
+    });
+      }
+       });
+    }
+  } catch (error) {
+    return message.reply(getLang("answer", error));
   }
 };
 
 module.exports = {
   config: {
     name: "gpt4o",
-    version: "1",
-    author: "null69",
+    version: "1.0",
+    author: "Null69",
     role: 0,
-    description: "Gpt4o 128k",
+    countDown: 5,
+    description: "Gpt4 Continuous conversation",
     category: "ai",
-    guide: "Please provide a prompt\n" + "{pn} text"
+    guide: "⚠ | Invalid Format!\n" + "Please provide a prompt: {pn} prompt"
   },
-  onStart: supot
-};
-const { GoatWrapper } = require('fca-liane-utils');
-const wrapper = new GoatWrapper(module.exports);
 
-wrapper.applyNoPrefix();
+  langs: {
+    en: {
+      answer: "◜gpt4o◞\n━━━━━━━━━━━━━━━━━━\n" + "%1\n━━━━━━━━━━━━━━━━━━",
+      usage: "◜gpt4o◞\n━━━━━━━━━━━━━━━━━━\n" + "❌ | Invalid Format\nPlease provide a message.\n━━━━━━━━━━━━━━━━━━"
+    }
+  },
+
+  onStart: async function({ event, message, args, commandName, api, getLang }) {
+    await gpt4o({ event, message, args, commandName, api, getLang });
+  },
+
+  onReply: async function({ Reply, event, message, args, commandName, api, getLang }) {
+    const { author } = Reply;
+    if (author != event.senderID) {
+      return;
+    }
+
+    await gpt4o({ event, message, args, commandName, api, getLang });
+  }
+};
