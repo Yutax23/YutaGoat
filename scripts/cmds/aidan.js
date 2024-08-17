a00f683a2d508d6831f4a4827a205a1a52f78c78
Yutax23 <junaydx23@yandex.com>
2024-08-18 00:46:07 +0800

    Update aidan.js

--- scripts/cmds/aidan.js
+++ scripts/cmds/aidan.js
@@ -42,7 +42,7 @@ module.exports = {
     try {
       const uid = event.senderID;
       const response = await axios.get(
-        `https://for-devs.onrender.com/api/gpt?query=${encodeURIComponent(prompt)}&uid=${uid}aidan&jbprompt=${jb}&apikey=api1`
+        `https://for-devs.onrender.com/api/gpt?query=${encodeURIComponent(prompt)}&uid=${uid}&jbprompt=${encodeURIComponent(jb)}&apikey=r-fbe7302b0890394458fed07c`
       );
 
       if (response.data && response.data.result) {
