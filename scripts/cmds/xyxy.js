2f3c79977d154ea361a3e9d948d69ab2f6f1db9f
Yutax23 <junaydx23@yandex.com>
2024-08-25 23:49:56 +0800

    Update xyxy.js

--- scripts/cmds/xyxy.js
+++ scripts/cmds/xyxy.js
@@ -13,7 +13,7 @@ try {
       }
     });
 
-    if (res.data.result) {
+    if (res.data.gpt4) {
       message.reaction("✅", event.messageID);
       const text = res.data.response;
        message.reply(getLang("answer", text), (err, info) => {
