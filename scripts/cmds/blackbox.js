ebdbda9703127c8ab790558ec115c22a46c6f0d7
Yutax23 <junaydx23@yandex.com>
2024-08-28 16:22:04 +0800

    Update blackbox.js

--- scripts/cmds/blackbox.js
+++ scripts/cmds/blackbox.js
@@ -1,65 +1,70 @@
-const axios = require("axios");
-const font = require("fontstyles");
-module.exports = {
-	config: {
-		name: 'blackbox',
-		version: '2.1.0',
-		author: 'KENLIEPLAYS',
-		countDown: 5,
-		role: 0,
-		shortDescription: 'Blackbox by Kenlie Navacilla Jugarap',
-		longDescription: {
-			en: 'Blackbox by Kenlie Navacilla Jugarap'
-		},
-		category: 'ai',
-		guide: {
-			en: '   {pn} <word>: ask with Blackbox'
-				+ '\n   Example:{pn} hi'
-		}
-	},
+async function xyxy({ event, message, args, commandName, api, getLang }) {
+  const prompt = args.join(" ");
+  if (!prompt) {
+    return message.reply(getLang("usage"));
+  }
+  const id = event.senderID;
+  const axios = require("axios");
+try {
+    const res = await axios.get(`https://deku-rest-api.gleeze.com/api/blackboxai`, {
+      params: {
+        q: encodeURIComponent(prompt),
+        uid: id
+      }
+    });
 
-	langs: {
-		en: {
-			chatting: 'Please wait...',
-			error: 'If this report spam please contact Kenlie Navacilla Jugarap'
-		}
-	},
-
-	onStart: async function ({ args, message, event, getLang }) {
-		if (args[0]) {
-			const yourMessage = args.join(" ");
-			try {
-				const responseMessage = await getMessage(yourMessage);
-				const rapjuga = `Is this answer helpful to you? Kindly click the link below
-https://click2donate.kenliejugarap.com
-(Clicking the link and clicking any ads or button and wait for 30 seconds (3 times) everyday is a big donation and help to us to maintain the servers, last longer, and upgrade servers in the future)`;
-				const lieken = responseMessage.replace(rapjuga, "");
-	
-	function applyBoldToText(sukuna) {
-    const boldRegex = /\*\*(.*?)\*\*/g;
-    return sukuna.replace(boldRegex, (match, text) => font.bold(text));
-}
-
-const modifiedResponse = applyBoldToText(lieken);
-				return message.reply(`${modifiedResponse}`);
-			}
-			catch (err) {
-				console.log(err)
-				return message.reply(getLang("error"));
-			}
-		}
-	},
+    if (res.data.result) {
+      message.reaction("✅", event.messageID);
+      const text = res.data.result;
+       message.reply(getLang("answer", text), (err, info) => {
+        if (!err) {
+    global.GoatBot.onReply.set(info.messageID, {
+      commandName,
+      author: id,
+      messageID: info.messageID
+    });
+      }
+       });
+    }
+  } catch (error) {
+    return message.reply(getLang("answer", error));
+  }
 };
 
-async function getMessage(yourMessage, langCode) {
-	try {
-		const res = await axios.get(`https://api.kenliejugarap.com/blackbox/?text=${yourMessage}`);
-		if (!res.data.response) {
-			throw new Error('Please contact Kenlie Navacilla Jugarap if this error spams...');
-		}
-		return res.data.response;
-	} catch (err) {
-		console.error('Error while getting a message:', err);
-		throw err;
-	}
-}
+module.exports = {
+  config: {
+    name: "blackbox",
+    version: "1.0",
+    author: "Null69",
+    role: 0,
+    countDown: 5,
+    description: "Gpt4 Continuous conversation",
+    category: "ai",
+    guide: "⚠ | Invalid Format!\n" + "Please provide a prompt: {pn} prompt"
+  },
+
+  langs: {
+    en: {
+      answer: "◜blackbox◞\n━━━━━━━━━━━━━━━━━━\n" + "%1\n━━━━━━━━━━━━━━━━━━",
+      usage: "◜blackbox◞\n━━━━━━━━━━━━━━━━━━\n" + "❌ | Invalid Format\nPlease provide a message.\n━━━━━━━━━━━━━━━━━━"
+    }
+  },
+
+  onStart: async function({ event, message, args, commandName, api, getLang }) {
+    await xyxy({ event, message, args, commandName, api, getLang });
+  },
+
+  onReply: async function({ Reply, event, message, args, commandName, api, getLang }) {
+    const { author } = Reply;
+    if (author != event.senderID) {
+      return;
+    }
+
+    await xyxy({ event, message, args, commandName, api, getLang });
+  }
+};
+
+const { GoatWrapper } = require('fca-liane-utils');
+const wrapper = new GoatWrapper(module.exports);
+
+wrapper.applyNoPrefix();
