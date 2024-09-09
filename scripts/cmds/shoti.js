37598dcd6310924194b49f188b818414b4896b7a
Yutax23 <junaydx23@yandex.com>
2024-09-09 09:40:53 +0800

    Update shoti.js

--- scripts/cmds/shoti.js
+++ scripts/cmds/shoti.js
@@ -12,9 +12,9 @@ module.exports.config = {
 module.exports.onStart = async function ({ message: m, event: e }) {
 m.reaction("⏱", e.messageID, () => {}, true);
 const a = require("axios");
-const b = await a.get(`https://shoti-srv2-itslibyzxy0.koyeb.app/api/v1/request-f`);
+const b = await a.get(`https://shoti-api.adaptable.app/api/v1/request-f`);
 const c = await global.utils.getStreamFromURL(b.data.data.url);
-const d = `🌸| Random Girl\n\nusername: ${b.data.data.user.username}\nnickname: ${b.data.data.user.nickname}\nuserid:${b.data.data.user.userID}`;
+const d = `🌸| Random Shoti Para kay Kape\n\nusername: ${b.data.data.user.username}\nnickname: ${b.data.data.user.nickname}\nuserid:${b.data.data.user.userID}`;
 
 m.reply({ body: d, attachment: c });
 await m.reaction("✅", e.messageID, () => {}, true);
